3612ec048d8933b3f7eeddfd87d93834fe7af99f
Amandeep Kaur <amankang344@gmail.com>
2024-06-10 20:50:35 -0400

    Setup the newtons cradle simulation

--- newtons-cradle/newtonsCradle.js
+++ newtons-cradle/newtonsCradle.js
@@ -1,4 +1,6 @@
 var canvas;
+var cradles;
+
 document.addEventListener("DOMContentLoaded", function(){
     // The canvas for the cradle
     canvas = document.createElement("div");
@@ -12,7 +14,6 @@ function startSimulation(){
     canvas.innerHTML = "";
 
     // Drawing the cradles inside canvas
-    var balls = [];
     for (var i = 0; i < document.getElementById('balls').value; i++) {
         // Drawing the cradle inside the canvas
         var cradle = document.createElement("div");
@@ -22,42 +23,47 @@ function startSimulation(){
         cradle.style.width = "1%";
         cradle.style.height = "50%";
         cradle.style.top = "0";
-        cradle.style.left = 20 + i * 10 + "%";
+        cradle.style.left = 20 + i * 6 + "%";
         cradle.style.transformOrigin = "top";
         cradle.style.transition = "transform 1s";
-        canvas.appendChild(cradle);
-        
+
         // Drawing balls
         var ball = document.createElement("div");
         ball.className = "ball";
         ball.style.position = "absolute";
         ball.style.backgroundColor = "black";
-        ball.style.width = "5%";
-        ball.style.height = "5%";
+        ball.style.width = "200%";
+        ball.style.height = "7%";
         ball.style.borderRadius = "50%";
-        ball.style.top = "50%";
-        ball.style.left = 18 + i * 10 + "%";
-        canvas.appendChild(ball);
-        balls.push(ball);
-        
+        ball.style.top = "98%";
+        ball.style.left = "10%";
+        ball.style.transform = "translateX(-25%)";
+        cradle.appendChild(ball);
+
+        canvas.appendChild(cradle);
     }
 
-    // Start swinging the cradles
-    var cradles = document.querySelectorAll('.cradle');
-    cradles.forEach(c => {
-        var angle = 30;
-        var interval = 1000;
-        var swingInterval = setInterval(function() {
-            c.style.transform = `rotate(${angle}deg)`;
-            setTimeout(function() {
-                c.style.transform = `rotate(${-angle}deg)`;
-            }, interval/2);
-            angle -= 10;
-            
-            if (angle <= 0) {
-                clearInterval(swingInterval);
-            }
-        }, interval);
-    });
-    
+    // Start swinging the cradle
+    cradles = document.querySelectorAll('.cradle');
+    startNextCradle(0);
+}
+
+// This moves the next cradle when the first one touches it
+function startNextCradle(index) {
+    var angle = index == 0 ? 30 : 20;
+    var interval = 1000;
+    var swingInterval = setInterval(function() {
+        // In case of first cradle, it will go to left first and then right. In case of the next cradles, it will go to right first and then left
+        cradles[index].style.transform = index == 0 ? `rotate(${angle}deg)` : `rotate(${-angle}deg)`;
+        if(index + 1 < document.getElementById('balls').value){
+            startNextCradle(index + 1);
+        }
+        setTimeout(function() {
+            cradles[index].style.transform = index == 0 ? `rotate(${-angle}deg)` : `rotate(${angle}deg)`;
+        }, interval/2);
+        angle -= 10;
+        if (angle <= 0) {
+            clearInterval(swingInterval);
+        }
+    }, interval);
 }
